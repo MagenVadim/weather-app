9be91573c0368a0d68f60663145f6e9e30dbe7c2
Vadim Smagin <magenvadim@gmail.com>
2024-03-21 03:25:39 +0200

    #3 Header component has added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { Routes ,Route } from 'react-router-dom'
 import { Home } from './pages/Home/components/Home';
 import { MonthStatistics } from './pages/MonthStatistics/components/MonthStatistics';
 
+import { Header } from './shared/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/month-statistics' element={<MonthStatistics/>}/>
