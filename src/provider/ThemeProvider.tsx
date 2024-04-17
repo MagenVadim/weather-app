7d708a955507bac89a5c47f7c0c254636c860d11
Vadim Smagin <magenvadim@gmail.com>
2024-04-18 02:11:53 +0300

    #21 ThemeProvider has added

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -1,10 +1,22 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import {ThemeContext} from "../context/ThemeContext"
+import { useState } from 'react'
 
 interface Props {
     children: ReactNode
 }
 
-export const ThemeProvider = ({children, ...props}: Props) =>{
 
-    return <div>{children}</div>
+export const ThemeProvider = ({children, ...props}: Props) =>{
+    const [theme, setTheme] = useState<string>('light');
+
+    function changeTheme (theme:string){
+        setTheme(theme)
+    }
+
+    return <ThemeContext.Provider value={{
+        theme,
+        changeTheme,
+    }}>{children}</ThemeContext.Provider>
+
 }
