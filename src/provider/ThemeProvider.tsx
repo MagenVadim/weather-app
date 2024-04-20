33fffeb9114805c96bc0091a134d5e3c6f264146
Vadim Smagin <magenvadim@gmail.com>
2024-04-20 23:56:08 +0300

    #24 LocalStorage has completed

--- src/provider/ThemeProvider.tsx
+++ src/provider/ThemeProvider.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 import {Theme, ThemeContext} from "../context/ThemeContext"
 import { useState } from 'react'
 import { changeCssRootVariables } from "../model/ChangeCssRootVariables";
+import {storage} from '../model/Storage'
 
 interface Props {
     children: ReactNode
@@ -9,9 +10,13 @@ interface Props {
 
 
 export const ThemeProvider = ({children, ...props}: Props) =>{
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+    const [theme, setTheme] = useState<Theme>(
+        storage.getItem('them') || Theme.LIGHT);
+    
+    changeCssRootVariables(theme)
 
     function changeTheme (theme: Theme){
+        storage.setItem('theme', theme)
         setTheme(theme);
         changeCssRootVariables(theme)
     }
