d27514f0cda900c03fab1ffe85bc841d9e85b1f1
Vadim Smagin <magenvadim@gmail.com>
2024-04-29 23:51:17 +0300

    #34 useustomelector has added

--- src/store/thunks/fetchCurrentWeater.ts
+++ src/store/thunks/fetchCurrentWeater.ts
@@ -4,11 +4,16 @@ import { AppDispatch } from "../store";
 
 export const fetchCurrentWeater = 
     (payload: string) => async (dispatch:AppDispatch) => {
-        dispatch(currentWeatherSlise.actions.fetchCurrentWeather());
-        const res = await WeatherService.getCurrentWeather(payload);
-        if(res.status===200){
-            dispatch(currentWeatherSlise.actions.fetchCurrentWeatherSuccess(res));
-        } else {
-            dispatch(currentWeatherSlise.actions.fetchCurrentWeatherError(res));
+        try{
+            dispatch(currentWeatherSlise.actions.fetchCurrentWeather());
+            const res = await WeatherService.getCurrentWeather(payload);
+            if(res.status===200){
+                dispatch(currentWeatherSlise.actions.fetchCurrentWeatherSuccess(res));
+            } else {
+                dispatch(currentWeatherSlise.actions.fetchCurrentWeatherError(res));
+            }
+        } catch (error){
+            console.log(error)
         }
+       
 }
